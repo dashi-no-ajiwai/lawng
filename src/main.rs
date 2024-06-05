d4222d06bfe4e33e752de914830ae68c435354fb
mizuochi hikaru <h_mizuochi@codmon.co.jp>
2024-06-05 11:47:28 +0900

    add clap

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use clap::Parser;
+
+/// Simple program to greet a person
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    /// The path to the file to read, with extension (e.g. `file.law`)
+    script: String,
+
+    /// The path to the directory to read sub-routine files from
+    #[arg(short, long)]
+    dir_path: String,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
 }
